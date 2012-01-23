b5cbc88d232f167d660abe91154aaf50347099ae
Jernej Krempuš <jkrempus@gmail.com>
2012-01-23 09:21:58 +0100

    Removing a script.

--- runall.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-for i in {4..22} 
-do 
-	echo -en "$i\t"; ./$1 $i
-done
--- test.d
+++ test.d
@@ -1,4 +1,4 @@
-//          Copyright Jernej Krempuš 2011 - 2012
+//          Copyright Jernej Krempuš 2012
 // Distributed under the Boost Software License, Version 1.0.
 //    (See accompanying file LICENSE_1_0.txt or copy at
 //          http://www.boost.org/LICENSE_1_0.txt)
