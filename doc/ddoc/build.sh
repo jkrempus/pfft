382b71cb0be132dc3c09210c1ccab6f5e4516c11
Jernej Krempuš <jkrempus@gmail.com>
2012-07-19 03:38:13 +0200

    Changed doc/candydoc into a submodule.

--- doc/ddoc/build.sh
+++ doc/ddoc/build.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-dmd -c -o- -Df../pfft.pfft.html ../../pfft/pfft.d candy.ddoc modules.ddoc additional-macros.ddoc
-dmd -c -o- -Df../pfft.stdapi.html ../../pfft/stdapi.d candy.ddoc modules.ddoc additional-macros.ddoc
-dmd -c -o- -Df../pfft.clib.html clib.d candy.ddoc modules.ddoc additional-macros.ddoc
+dmd -c -o- -Df../pfft.pfft.html ../../pfft/pfft.d ../candydoc/candy.ddoc modules.ddoc additional-macros.ddoc
+dmd -c -o- -Df../pfft.stdapi.html ../../pfft/stdapi.d ../candydoc/candy.ddoc modules.ddoc additional-macros.ddoc
+dmd -c -o- -Df../pfft.clib.html clib.d ../candydoc/candy.ddoc modules.ddoc additional-macros.ddoc
